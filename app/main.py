84bde94dd07655a1cfbfa53073dae450182fe5d0
Tomas Muñoz <tomasvalentin013@gmail.com>
2025-06-14 20:10:19 -0300

    cambios en main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-from app.routers import producto
+from app.routers.productos import router as productos_router
 
 app = FastAPI()
 
-app.include_router(producto.router)
+app.include_router(productos_router)
 
