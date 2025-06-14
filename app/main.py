8296fbb7f62fb587847a740186080e64bd61e34c
Tomas Muñoz <tomasvalentin013@gmail.com>
2025-06-14 13:51:01 -0300

    cambios en main

--- app/main.py
+++ app/main.py
@@ -4,9 +4,7 @@ from app.database import Base, engine
 
 app = FastAPI(title="Tienda API")
 
-# Crear tablas
 Base.metadata.create_all(bind=engine)
 
-# Incluir rutas
 app.include_router(productos.router)
 
